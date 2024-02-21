252c186071a80a6854e5adfea0f212aad7ffcf1a
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-02-21 11:37:14 +0000

    Math operators

---
+++ MathOperators.swift
@@ -0,0 +1,24 @@
+//
+//  MathOperators.swift
+//
+//  Created by Kent Gatera
+//  Created on 2024-02-18
+//  Version 1.0
+//  Copyright (c) 2024 Kent Gatera. All rights reserved.
+//
+//  Using Swift math operators.
+
+import Foundation
+
+// Addition
+print("The sum of 2 + 3 is \(2 + 3)")
+// Subtraction
+print("The difference of 2 - 3 is \(2 - 3)")
+// Division
+print("10 divided by 6 is " + (String(10.0 / 6.0)))
+// Multiplication
+print("The product of 2 and 3 is " + (String(2*3)))
+// Power
+print("2 to the power of 3 is " + String(pow(Float(2),Float(3))))
+// Square root
+print("The root of 4 is " + String(sqrt(4)))
